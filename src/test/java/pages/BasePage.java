03447e1697b402dc4bf8ac9b071036394dc5647b
Yuschkewicz <13and.jei@gmail.com>
2022-04-18 17:18:35 +0300

    0.2

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -7,7 +7,7 @@ public class BasePage {
     public void setUp() {
         Configuration.baseUrl = "http://qase.io/";
         Configuration.browser = "chrome";
-        Configuration.headless = false;
+        Configuration.headless = true;
         Configuration.clickViaJs = true;
         Configuration.timeout = 10000;
     }
